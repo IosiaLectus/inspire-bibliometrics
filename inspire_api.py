4db2be84de95cb5001fb541995deb276fb09327b
josiah <josiah.couch@gmail.com>
2019-12-31 15:42:07 -0600

    added a few more functions

--- inspire_api.py
+++ inspire_api.py
@@ -18,6 +18,18 @@ def inspire_search(query,ot=False):
     url += '&rg=25000'
     page = requests.get(url)
     data = page.json()
+    #print("\n"+url+"\n")
+    return data
+
+# Retrieve record from inspirehep.net by recid
+def inspire_record(recid,ot=False):
+    url = 'https://inspirehep.net/record/{}?of=recjson'\
+            .format(recid)
+    if ot:
+        url += '&ot={}'.format(ot)
+    page = requests.get(url)
+    data = page.json()
+    #print("\n"+url+"\n")
     return data
 
 # Get the set of authors who cite a given author, all identified by their author ids
@@ -29,9 +41,32 @@ def unique_citing_authors(author_id):
 
 # Get the number of authors on a certain work
 def NAuthors(recid):
-    data = inspire_search('recid:{}'.format(recid),'authors')
+    data = inspire_record(recid,'authors')
     auth_list = [auth for d in data for auth in d['authors']]
     return(len(auth_list))
+
+# Get number of citations from a paper
+def NCitations(recid):
+    #data = inspire_search('refersto:recid:{}'.format(recid),'reference')
+    #return len(data)
+    record = inspire_record(recid,"number_of_citations")
+    if len(record)<1:
+        return 0
+    return record[0]["number_of_citations"]
+
+# Get citations for each paper by an author
+def CitationsByPaper(author_id):
+    data = inspire_search('author:{}'.format(author_id),'title,recid')
+    papers = [(d['title']['title'],d['recid']) for d in data]
+    ret = sorted([{'title':p[0], 'recid':p[1], 'citations':NCitations(p[1])} for p in papers], key=lambda d: d['citations'], reverse=True)
+    return ret
+
+# Get recid by title
+def recid_from_title(title):
+    data = inspire_search("title:'{}'".format(title),'recid')
+    if len(data)<1:
+        return -1
+    return data[0]['recid']
 
 ################################################################################
 # Below I attempt to implement the citation coin metric of arxiv:1803.10713
@@ -69,8 +104,78 @@ def TopPapersAuthCoin(author_id, n):
 def PositiveAuthCoin(author_id):
     plist = AuthCoinByPaper(author_id)
     clist = list(filter(lambda x: x>0, [p['authcoin']/p['Nauthors'] for p in plist]))
-
     return sum(clist)
+
+################################################################################
+# A few things I want to define myself
+################################################################################
+
+# Get citations which each in turn have at least 10 citations (or go deeper with n>1)
+# In more detail, for n=0 simply return the citation count
+# For n=1, return the number of citations such that the citing paper has at least k citations.
+# For n>1, return the number of citations such that the citing paper has at an i10(n-1,k) score of at least k.
+# This is inspired by the i10 score defined for authors
+def i10_citations(recid,n,k=10):
+    if n<1:
+        return NCitations(recid)
+    data = inspire_search('refersto:recid:{}'.format(recid),'recid,number_of_citations')
+    data = [d for d in data if d]
+    print(data)
+    print()
+    data2 = []
+    if n==1:
+        data2 = [d['number_of_citations'] for d in data]
+    else:
+        data2 = [i10_citations(d['recid'],n-1,k) for d in data]
+    print(data2)
+    print()
+    data2 = [x for x in data2 if x>k]
+    return len(data2)
+
+# Just as the above is a recursive i10 score for papers, here we define a recursive h-index for papers.
+def h_index_citations(recid,n):
+    if n<1:
+        return NCitations(recid)
+    data = inspire_search('refersto:recid:{}'.format(recid),'recid,number_of_citations')
+    data = [d for d in data if d]
+    print(data)
+    print()
+    data2 = []
+    if n==1:
+        data2 = [d['number_of_citations'] for d in data]
+    else:
+        data2 = [h_index_citations(d['recid'],n-1) for d in data]
+    print(data2)
+    print()
+    h_index = 0
+    while len(data2)>h_index:
+        h_index += 1
+        data2 = [x for x in data2 if x>h_index]
+    return h_index
+
+# get all papers 'downstream' from the current paper in the citation graph, i.e. the paper's descendants.
+def get_descendants(recid):
+    data = inspire_search('refersto:recid:{}'.format(recid),'title,recid')
+    data = [d for d in data if d]
+    i = 0
+    while i < len(data):
+        print(i)
+        print()
+        this_paper = data[i]
+        if isinstance(this_paper, dict):
+            data.extend(inspire_search('refersto:recid:{}'.format(data[i]['recid']),'title,recid'))
+            # solution from https://www.geeksforgeeks.org/python-removing-duplicate-dicts-in-list/ to remove duplicates
+        data = [j for n, j in enumerate(data) if not j in data[n+1:]]
+        data = [d for d in data if d]
+        i += 1
+        print(len(data))
+        print()
+    data = sorted(data, key=lambda x: x['recid'])
+    return data
+
+# get total number of descendants.
+def number_of_descendants(recid):
+    return len(get_descendants(recid))
 
 # Actually do stuff
 def main():
@@ -92,16 +197,24 @@ def main():
 
 
     TGProfs = ['E.Caceres.1','J.Distler.1','W.Fischler.1','V.S.Kaplunovsky.1','Can.Kilic.1','R.A.Matzner.1','S.Paban.1','A.B.Zimmerman.1']
-    OtherPeople = ['J.Couch.1','H.Marrochio.1','Ming.Lei.Xiao.1','Phuc.H.Nguyen.1','J.F.Pedraza.1']
+    OtherPeople = ['Josiah.D.Couch.1','H.Marrochio.1','Ming.Lei.Xiao.1','Phuc.H.Nguyen.1','J.F.Pedraza.1']
+
+    print()
+    #print(recid_from_title("Noether charge, black hole volume, and complexity"))
+    #print()
+    #print(inspire_record('1681268'))
+    recid = recid_from_title("Noether charge, black hole volume, and complexity")
+    print(get_descendants('4181'))
+    print()
 
     #print(NicitP('451647'))
     #print(NAuthors('1681268'))
-    papers = AuthCoinByPaper('J.Couch.1')
-    print(papers)
-    for p in OtherPeople:
-        print("id:{}, authcoin:{}".format(p,AuthCoin(p)))
-        time.sleep(0.5)
-    print()
+    #papers = AuthCoinByPaper('Josiah.D.Couch.1')
+    #print(papers)
+    # for p in OtherPeople:
+    #    print("id:{}, authcoin:{}".format(p,AuthCoin(p)))
+    #    time.sleep(0.5)
+    # print()
 
 if __name__ == '__main__':
 	main()
